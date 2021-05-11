2da03430c158ce90c42c4231355ffeb7db4ff351
jarod <jcervantesg05@gmail.com>
2021-05-10 18:59:26 -0600

    inicializa

--- Simulador.h
+++ Simulador.h
@@ -31,17 +31,21 @@ struct Simulador {
 
 //recorre las m simulaciones
 void hacerSimulaciones (struct Simulador *simulador){
-    for (int i = 0 ; i<=simulador->m; i++ ){
+
+    for (int i = 0 ; i<simulador->m; i++ ){
+        printf(" m %d",i);
         tirarManos(simulador);
         //sumo los contadores a totales
         //y los inicializo
+        printf("\n");
     }
     //determino probabilidades
 }
 
 //recorre las n manos
 void tirarManos (struct Simulador *simulador){ //
-    for (int i = 0 ; i<=simulador->n; i++ ){
+    for (int i = 0 ; i<simulador->n; i++ ){
+            printf(" n %d",i);
         //asumo que el mazo ya está formado
         //llamo a la funcion que me da 5 cartas
         //llamo a la funcion que determina cual jugada es
@@ -51,6 +55,38 @@ void tirarManos (struct Simulador *simulador){ //
 }
 
 void identificarMano (){
+
+}
+
+void cargarDatos(struct Simulador *simulador){
+    simulador->countDoubles = 0;
+    simulador->countFull = 0;
+    simulador->countPoker = 0;
+    simulador->countRoyal = 0;
+
+    simulador->countDoubles_total = 0;
+    simulador->countFull_total = 0;
+    simulador->countPoker_total = 0;
+    simulador->countRoyal_total = 0;
+
+    simulador->cantidadManos = 1;
+
+    printf("Programa para simular eventos probabilisticos en C\n");
+    printf("Ingrese el numero de Simulaciones : \n");
+    scanf("%d",&simulador->m);
+    printf("Ingrese el numero de Manos : \n");
+    scanf("%d",&simulador->n);
+    printf("m es : %d\n", simulador->m);
+    printf("n es : %d\n", simulador->n);
+
+    //tambien inicializa la mano
+
+
+}
+
+void iniciarSimulacion(struct Simulador *simulador){
+    cargarDatos(simulador);
+    hacerSimulaciones(simulador);
 
 }
 #endif // SIMULADOR_H_INCLUDED
---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Mazo.h"
+#include "Simulador.h"
+#include "Includes.h"
+int main()
+
+{
+    struct Simulador s;
+    iniciarSimulacion(&s);
+
+    return 0;
+}
