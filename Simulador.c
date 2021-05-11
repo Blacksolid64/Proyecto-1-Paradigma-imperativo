42cbc7905d1b723eb10a1688aee30b49576d2ebe
LuisVenegasLeiva <70867079+LuisVenegasLeiva@users.noreply.github.com>
2021-05-10 19:55:02 -0600

    Update Simulador.c

--- Simulador.c
+++ Simulador.c
@@ -7,13 +7,17 @@ int main()
 {
     int m;
     struct Mazo mimazo[CARTAS];
+    struct Mano mimano[CARTAS];
 
     establecerMazo(mimazo);
-    mostrarCartas(mimazo);
-    sacarCartas(mimazo);
+    //mostrarCartas(mimazo);
+    sacarCartas(mimazo,mimano);
+    isroyal(mimano);
+    isfullhouse(mimano);
+    isquad(mimano);
+    isdouble(mimano);
     printf("Programa para simular eventos probabilisticos en C\n");
     printf("Ingrese el numero de Simulaciones\n");
     scanf("%d",&m);
     return 0;
 }
-
